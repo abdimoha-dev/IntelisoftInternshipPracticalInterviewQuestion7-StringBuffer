d9cfd856fb4768b28a3a30b82b25a83621a32796
Etyang Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2019-07-09 00:49:02 +0300

    With the help of code snippets, distinguish between String, StringBuilder, and StringBuffer.

---
+++ src/stringbuffertest/StringBufferTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbuffertest;
+
+/**
+ *
+ * @author Abdi Mohammed
+ */
+public class StringBufferTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StringBuffer buffer=new StringBuffer("Intelli");  
+        buffer.append("Soft");  
+        System.out.println(buffer);
+    }
+    
+}
